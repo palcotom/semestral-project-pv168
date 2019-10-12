738e744af9ea78d534722d6ab5cd2be07a253deb
Oskar Špaček <xspacek3@fi.muni.cz>
2019-10-13 01:36:40 +0200

    GUI set up for work
    initial commit on this branch

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * @author Oskar Spacek
  */
-public class MainWindow {
+public class MainWindow extends JFrame {
+
+    private MainWindow() {
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() ->
+                new MainWindow().setVisible(true));
+    }
 }
