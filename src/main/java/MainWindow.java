221736f6964174375ba83358d504e1827940d55c
Oskar Špaček <xspacek3@fi.muni.cz>
2019-10-13 17:36:02 +0200

    Toolbar

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -9,6 +9,24 @@ public class MainWindow extends JFrame {
     private MainWindow() {
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JToolBar toolBar = new JToolBar();
+        add(toolBar, BorderLayout.BEFORE_FIRST_LINE);
+
+        /**
+        JButton exitButton = new JButton("Exit");
+        exitButton.addActionListener(actionEvent -> System.exit(0));
+         */
+
+        JButton addButton = new JButton("Add");
+        JButton filterButton = new JButton("Filter");
+        JButton removeButton = new JButton("Remove");
+
+        //toolBar.add(exitButton);
+        toolBar.add(addButton);
+        toolBar.add(filterButton);
+        toolBar.add(removeButton);
+
     }
 
     public static void main(String[] args) {
